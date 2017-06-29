c1b4cd9feea172ec32af79c87c8f39732cc2c297
Aivin V. Solatorio <avsolatorio@gmail.com>
2017-06-29 11:36:34 +0800

    Include a script to run the slides locally. Listen to all IP.

--- launch_local_server.sh
+++ launch_local_server.sh
@@ -1,1 +1,2 @@
+# https://github.com/jupyter/nbconvert/issues/102#issuecomment-141132480
 jupyter-nbconvert --to slides slides.ipynb --reveal-prefix=reveal.js --post serve --ServePostProcessor.ip='*'
